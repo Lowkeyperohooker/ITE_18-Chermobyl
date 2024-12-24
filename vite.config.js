6a1dc83f1770f11b8d4e59434e7bc24157bcff51
Lowkeyperohooker <123971143+Lowkeyperohooker@users.noreply.github.com>
2024-12-24 12:39:36 +0000

    Basta

--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,31 @@
-const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env
+import { defineConfig } from 'vite';
+import path from 'path';
 
-export default {
-    root: 'src/',
-    publicDir: '../static/',
-    base: './',
-    server:
-    {
-        host: true,
-        open: !isCodeSandbox // Open if it's not a CodeSandbox
+// Detect if the environment is CodeSandbox
+const isCodeSandbox = 'SANDBOX_URL' in process.env || 'CODESANDBOX_HOST' in process.env;
+
+export default defineConfig({
+  root: 'src/',
+  publicDir: '../static/',
+  base: './',
+  server: {
+    host: true,
+    open: !isCodeSandbox, // Open browser if not in CodeSandbox
+  },
+  build: {
+    outDir: '../dist',
+    emptyOutDir: true,
+    sourcemap: true,
+    rollupOptions: {
+      // Resolve three/examples/jsm issues
+      external: ['three/examples/jsm/loaders/GLTFLoader.js'], // Treat as external
     },
-    build:
-    {
-        outDir: '../dist',
-        emptyOutDir: true,
-        sourcemap: true
-    }
-}
+  },
+  resolve: {
+    resolve: {
+      alias: {
+        'three/examples/jsm': path.resolve(__dirname, 'node_modules/three/examples/jsm'),
+      },
+    },
+  },
+});
